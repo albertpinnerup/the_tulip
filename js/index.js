b9cc154c68d1e5eb65740241015b6584194a23ae
Albert Pinnerup <albert.pinnerup@gmail.com>
2025-02-05 14:11:55 +0100

    working on the rooms section

--- js/index.js
+++ js/index.js
@@ -3,12 +3,17 @@
 let root =  document.querySelector("#root");
 
 let header = document.createElement("header");
+header.classList.add("header")
 
 let main = document.createElement("main");
 
 let footer = document.createElement("footer");
 
+
+
 root.append(header, main, footer);
 
-
+function highLightText(sourceTxt, highLightTxt) {
+    return sourceTxt.replace(highLightTxt, `<span class=highlight__text>${highLightTxt}</span>`)
+}
 
