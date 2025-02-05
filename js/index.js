aa111226c15fbdb16c075427d87a7bf4520513d3
Albert Pinnerup <albert.pinnerup@gmail.com>
2025-02-05 09:38:12 +0100

    initialised html document

---
+++ js/index.js
@@ -0,0 +1,9 @@
+let root =  document.querySelector("#root");
+
+let header = document.createElement("header");
+
+let main = document.createElement("main");
+
+let footer = document.createElement("footer");
+
+root.append(header, main, footer)
